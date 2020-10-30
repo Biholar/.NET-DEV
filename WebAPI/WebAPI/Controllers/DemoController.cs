4c09443e4e7777f6119736a763a0074c8b2e37c9
Alex Biholar <Biholar0372@gmail.com>
2020-10-30 23:37:41 +0200

    Initial Commit

---
+++ WebAPI/WebAPI/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public string Get()
+        {
+            return "Welcome to WEB API";
+        }
+        public List <string> Get(int Id)
+        {
+            return new List<string>
+            {
+                "Data1",
+                "Data2"
+            };
+        }
+    }
+}
